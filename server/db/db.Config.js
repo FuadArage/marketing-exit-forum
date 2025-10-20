d75ec6f5e7bebdf792359bcc899aa90b6a95bc69
fuaddot <fuadarage10@gmail.com>
2025-10-20 09:45:32 +0300

    Prepare server for deployment

--- server/db/db.Config.js
+++ server/db/db.Config.js
@@ -4,7 +4,7 @@ require("dotenv").config(); // Load environment variables from .env
 //using .env for sensetive info
 
 const dbconnection = mysql2.createPool({
-  host: "localhost",
+  host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
